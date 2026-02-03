9015df0fb7a81c2028bb2c7382565066755de429
neefmadeit <aneef_s@yahoo.com>
2026-02-02 19:55:35 -0500

    First Pass

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,22 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    """
+    Represents a book with an author and a title.
+    """
+    def __init__(self, author="", title=""):
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        """
+        Prints a string representing the book in the format 'title, written by author'.
+        """
+        print(f"{self.title}, written by {self.author}")
 
+# Instantiate two objects from the Book class
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
-if __name__ == "__main__":
-    a = Book("Thorpe", "Beat the Dealer")
-    print("This is a assignment 1 part 2")
+# Print both of these objects to the screen by calling their display() functions
+print("Displaying book details:")
+book1.display()
+book2.display()
