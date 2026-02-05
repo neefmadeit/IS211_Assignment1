4cdce50b7f691f38affc46c989d1cce0227bf0f4
neefmadeit <aneef_s@yahoo.com>
2026-02-04 20:37:27 -0500

    Polished.

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,22 +1,16 @@
 class Book:
-    """
-    Represents a book with an author and a title.
-    """
+
     def __init__(self, author="", title=""):
         self.author = author
         self.title = title
 
     def display(self):
-        """
-        Prints a string representing the book in the format 'title, written by author'.
-        """
+
         print(f"{self.title}, written by {self.author}")
 
-# Instantiate two objects from the Book class
 book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
 book2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
-# Print both of these objects to the screen by calling their display() functions
 print("Displaying book details:")
 book1.display()
 book2.display()
